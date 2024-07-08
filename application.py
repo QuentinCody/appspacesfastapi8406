55e07674320b520f5daa33f9b25e6b0bdd829f11
QuentinCody <QuentinCody@gmail.com>
2024-07-08 14:52:43 -0400

    chore: Add new API endpoint for retrieving transactions with details

--- application.py
+++ application.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/transactions_with_details?page=1&page_size=20&sort_order=asc&timezone=America%2FNew_York HTTP/1.1")
+async def get_transactions_with_details_test():
+    return ("message": "2nd API endpoint works!")
