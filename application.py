4263b3ad20168f2b3cddf3649457f53686c3c225
QuentinCody <QuentinCody@gmail.com>
2024-07-08 14:56:46 -0400

    chore: Refactor API endpoint for retrieving transactions with details

--- application.py
+++ application.py
@@ -7,6 +7,6 @@ async def read_root():
     return {"message": "Hello, World!"}
 
 
-@app.get("/transactions_with_details?page=1&page_size=20&sort_order=asc&timezone=America%2FNew_York HTTP/1.1")
+@app.get("/transactions/daily-totals")
 async def get_transactions_with_details_test():
     return {"message": "2nd API endpoint works!"}
